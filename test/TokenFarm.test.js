fe374399fe3d941278b18aa4239da1afe3c76429
realkelvinperez <realkelvinperez@gmail.com>
2021-03-10 13:39:51 -0500

    added test for mDai, dapp, & tokenfarm contracts and checking to see correct init balance was transferred

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -1,3 +1,4 @@
+const web3 = require("web3");
 const TokenFarm = artifacts.require("TokenFarm");
 const DaiToken = artifacts.require("DaiToken");
 const DappToken = artifacts.require("DappToken");
@@ -6,12 +7,48 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('TokenFarm', (accounts) => {
-   describe('Mock Dai Deployment', async () => {
+function token(n) {
+    return web3.utils.toWei(n, 'ether');
+}
+
+contract('TokenFarm', ([owner, investor]) => {
+    let daiToken, dappToken, tokenFarm;
+
+    before(async () => {
+        daiToken = await DaiToken.new();
+        dappToken = await DappToken.new();
+        tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address);
+
+        // Transfer all Dapp token to te farm(1 Million)
+        await dappToken.transfer(tokenFarm.address, token('1000000'));
+
+        // Transfer Investor Tokens
+        await daiToken.transfer(investor, '100000000000000000000', { from: owner });
+    })
+
+    describe('Mock Dai Deployment', async () => {
        it('has a name', async () => {
            let daiToken = await DaiToken.new();
            const name = await daiToken.name();
            assert.equal(name, 'Mock DAI Token');
        })
-   })
+    })
+
+    describe('Dapp Token Deployment', async () => {
+        it('has a name', async () => {
+            const name = await dappToken.name();
+            assert.equal(name, 'DApp Token');
+        })
+    })
+
+    describe('Token Farm Deployment', async () => {
+        it('has a name', async () => {
+            const name = await tokenFarm.name();
+            assert.equal(name, 'Dapp Token Farm');
+        })
+        it('contract has tokens', async () => {
+            let balance = await dappToken.balanceOf(tokenFarm.address);
+            assert.equal(balance.toString(), token('1000000'));
+        })
+    })
 })
